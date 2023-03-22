d582a4167b99811577cc72ea153e6f58899a398b
Stanislav Sakun <Stanislav.sakun@ukr.net>
2023-03-22 14:10:01 +0200

    fix basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <BrowserRouter basename="/">
         <Suspense fallback={<Fallback />}>
           <PersistGate loading={null} persistor={persistor}>
             <App />
